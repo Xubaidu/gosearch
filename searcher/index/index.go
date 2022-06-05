792cf9608ec8315ed5069445f8dc3d3e9026a0af
ChenyangXu <1127862434@qq.com>
2022-06-05 16:55:33 +0800

    feat: add levelDB

---
+++ searcher/index/index.go
@@ -0,0 +1,12 @@
+package index
+
+import (
+	"go-search/clients"
+)
+
+func InsertRevIndex(tokens []string, docID int64) error {
+	var DB = clients.DB
+	for _, token := range tokens {
+	}
+	return nil
+}
