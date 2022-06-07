dbd3b07ee9b3e3441fa16b13e0406ecbf65d052e
ziyefanx <2574736532@qq.com>
2022-06-07 18:40:49 +0800

    leveldb_v2

--- searcher/index/index.go
+++ searcher/index/index.go
@@ -14,25 +14,26 @@ type Index struct {
 }
 
 func InsertRevIndex(doc string) error {
+	var s = &storage.LeveldbStorage{}
 	// FIXME: add storage.GetDoc(doc) function
-	if _, err := storage.GetDoc(doc); err == nil {
+	if _, err := Lestorage.GetDoc(doc); err == nil {
 		err = errors.New("doc already exists")
 		log.Println(err)
 		return err
 	}
 	// FIXME: add GetDocNum() function
-	docID := storage.GetDocNum(doc) + 1
+	docID := Lestorage.GetDocNum(doc) + 1
 	tokens := word.Tokenizer(doc)
 	for _, token := range tokens {
 		var index Index
-		if err := storage.Get(token, &index); err != nil {
+		if err := s.Get(token, &index); err != nil {
 			index.DocList = []int64{docID}
 			index.Count = 1
 		} else {
 			index.DocList = append(index.DocList, docID)
 			index.Count++
 		}
-		err := storage.Set(token, &Index{
+		err := s.Set(token, &Index{
 			DocList: append(index.DocList, docID),
 			Count:   index.Count + 1,
 			Token:   token,
@@ -60,7 +61,7 @@ func Calc(tokens []string) ([]*Index, error) {
 	var indexes []*Index
 	for _, token := range tokens {
 		var index Index
-		err := storage.Get(token, &index)
+		err := s.Get(token, &index)
 		if err != nil {
 			log.Println(err)
 			return nil, err
--- storage/leveldb_test.go
+++ storage/leveldb_test.go
@@ -13,11 +13,12 @@ func TestInitLevelDB(t *testing.T) {
 			name: "test 1",
 		},
 	}
+	var s = &LeveldbStorage{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_ = InitLevelDB()
-			_ = DB.Put([]byte("key"), []byte("value"), nil)
-			data, _ := DB.Get([]byte("key"), nil)
+			_ = s.InitLevelDB()
+			_ = s.db.Put([]byte("key"), []byte("value"), nil)
+			data, _ := s.db.Get([]byte("key"), nil)
 			fmt.Println(string(data))
 		})
 	}
