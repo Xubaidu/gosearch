9e971fecc51767450d59278fbbf14b8c5ba0a779
ChenyangXu <1127862434@qq.com>
2022-06-12 17:00:33 +0800

    fix: use hard-coded relative path to avoid searcher building new dbs.

--- core/storage/init.go
+++ core/storage/init.go
@@ -5,7 +5,8 @@ var RevDocDB *LeveldbStorage
 var RevTokenDB *LeveldbStorage
 
 func init() {
-	ForwardDocDB = NewLeveldbStorage("forward_doc_db")
-	RevDocDB = NewLeveldbStorage("rev_doc_db")
-	RevTokenDB = NewLeveldbStorage("rev_token_db")
+	// TODO: use environment variable to replace the following hard-coded path
+	ForwardDocDB = NewLeveldbStorage("../storage/forward_doc_db")
+	RevDocDB = NewLeveldbStorage("../storage/rev_doc_db")
+	RevTokenDB = NewLeveldbStorage("../storage/rev_token_db")
 }
